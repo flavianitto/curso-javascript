1d10fc8001681d3ec13200db315ad9beea4dd0c3
Flavia Narumi Nitto <85723447+flavianitto@users.noreply.github.com>
2022-02-18 08:35:39 -0300

    laço for e debugger

---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+//estrutura de repetição com variavel de controle
+console.log('Iniciando o for...');
+for(var c=0; c<6; c++) {
+   // console.log(`[${c+1}] Esta funcionando com for`);
+   console.log(c);
+}
+console.log('Fim do for!');
+
+/* 
+  * usar o debugger 
+  * cria o breakpoint (bolinha vermelha)
+  * aperta f10 pra acompanhar o fluxo
+  * setinha amarela mostra qual comando está sendo executado na hora
+*/
